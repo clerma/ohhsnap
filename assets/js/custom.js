da38c2bf42e6a90a3dc9f901753a7d0661ae5e92
Carlos Lerma <carloslerma@me.com>
2024-01-04 09:31:05 -0600

    update to seo content and other site changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,3 +4,16 @@ if (isAuthenticated) {
   document.body.className += " authenticated";
 }
 
+  /* curator-feed-default-feed-layout */
+(function() {
+    var i,
+        e,
+        d = document,
+        s = "script";
+    i = d.createElement("script");
+    i.async = 1;
+    i.src = "https://cdn.curator.io/published/65cf3aa1-9855-4aa7-82af-12c174dffdad.js";
+    e = d.getElementsByTagName(s)[0];
+    e.parentNode.insertBefore(i, e);
+})();
+
