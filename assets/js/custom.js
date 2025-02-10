b252536bb615bd866a3320237e2fd7a9af02ed25
Carlos <carlos@accoladesit.com>
2025-02-10 12:05:57 -0600

    remove script

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -74,14 +74,3 @@ document.addEventListener('snipcart.ready', function() {
      })();
    }
  });
-// Passive event listeners
- jQuery.event.special.touchstart = {
-     setup: function( _, ns, handle ) {
-         this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
-     }
- };
- jQuery.event.special.touchmove = {
-     setup: function( _, ns, handle ) {
-         this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
-     }
- };
